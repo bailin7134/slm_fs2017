68a9102dd0303c504d68b02c24f896390440778c
bailin7134 <lbai.ee.ethz@gmail.com>
2017-03-13 00:20:39 +0100

    update code and report

--- Exercise1/solution.r
+++ Exercise1/solution.r
@@ -32,6 +32,10 @@ sd(caDataFrame$Education)
 sqrt(mean(caDataFrame$Wage^2) - mean(caDataFrame$Wage)^2)  # population standard devivation
 sqrt(mean(caDataFrame$Education^2) - mean(caDataFrame$Education)^2)
 
+#
+usSub<-subset(educationDataFrame, educationDataFrame$Country=="US")
+usDataFrame<-subset(usSub, usSub$Education>0 & (usSub$Gender==1 | usSub$Gender==2) & usSub$Wage>0)
+
 # question 4
 
 maleSub <- Gender=="1"
@@ -42,6 +46,9 @@ sd(maleDataFrame$Education)
 sqrt(mean(maleDataFrame$Wage^2) - mean(maleDataFrame$Wage)^2)  # population standard devivation
 sqrt(mean(maleDataFrame$Education^2) - mean(maleDataFrame$Education)^2)
 
+maleSub<-subset(educationDataFrame, educationDataFrame$Gender=="1")
+maleDataFrame<-subset(maleSub, maleSub$Education>0 & (maleSub$Gender==1 | maleSub$Gender==2) & maleSub$Wage>0)
+summary(maleDataFrame)
 
 femaleSub <- Gender=="1"
 femaleDataFrame <- educationDataFrame[femaleSub,]
@@ -51,3 +58,29 @@ sd(femaleDataFrame$Education)
 sqrt(mean(femaleDataFrame$Wage^2) - mean(femaleDataFrame$Wage)^2)  # population standard devivation
 sqrt(mean(femaleDataFrame$Education^2) - mean(femaleDataFrame$Education)^2)
 
+# filter all negative value in education
+educationDataFrame <- read.table("Education.txt", header=TRUE, sep="")
+
+iter = 1
+val = 1
+data_length = length(educationDataFrame$ID)
+
+while(iter <= data_length)
+{
+  # filter wrong education & gender
+  if (educationDataFrame$Education[val]<0)
+  {
+    print(val)
+    educationDataFrame = educationDataFrame[-c(val), ]
+  }
+  else if (educationDataFrame$Gender[val] != 1 & educationDataFrame$Gender[val] != 2)
+  {
+    print(val)
+    educationDataFrame = educationDataFrame[-c(val), ]
+  }
+  else
+    val = val + 1
+  
+  iter = iter + 1
+}
+summary(educationDataFrame)
