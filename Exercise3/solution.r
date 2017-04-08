c21404304502ba211bde1bea832994556b016749
bailin7134 <lbai.ee.ethz@gmail.com>
2017-04-08 10:54:14 +0200

    last question added

--- Exercise3/solution.r
+++ Exercise3/solution.r
@@ -41,5 +41,17 @@ generateSample <- function (n_samples, mean, stdev){
 }
 
 # question 4
-x <- generateSample(20, 2, 1.5)
-t.test(x, mu=1.5, alternative='two.sided')
+testList <- list()
+totalSample <- c()
+for(i in 1:20) {
+  x <- generateSample(20, 2, 1.5)
+  result <- t.test(x, mu=1.5, alternative='two.sided')
+  totalSample <- append(totalSample, x)
+  
+  testList[[i]] <- result
+}
+# print(testList)
+# the mean = 1.5 for each smaples varies. Some of them are true but other are not. 
+
+t.test(totalSample, mu=1.5, alternative='two.sided')
+# if get together all the samples, the mean is almost 1.5
