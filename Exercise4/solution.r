7e73074fead26dc219beaf708a1e1bdaa65690c1
bailin7134 <lbai.ee.ethz@gmail.com>
2017-04-11 09:54:12 +0200

    solution to exercise 4

--- Exercise4/solution.r
+++ Exercise4/solution.r
@@ -12,9 +12,9 @@ mld=lm(formula = Education~Wage, data=mEduDataFrame)
 lm(formula = Education~Wage, data=fEduDataFrame)
 
 xMale <- seq(1:25)
-yMale <- x*398.5+24
+yMale <- xMale*398.5+24
 xFemale <- seq(1:25)
-yFemale <- x*397.5-563.5
+yFemale <- xFemale*397.5-563.5
 lines(xMale, yMale, col="blue")
 lines(xFemale, yFemale, col="red")
 
@@ -25,28 +25,50 @@ lines(xFemale, yFemale, col="red")
 # the linear model for female is:
 # wage = eduction * 397.5 - 563.5
 
-# Q2:
-print("Answer of question 2")
-pcDateFrame <- read.table("ComputerData.txt", header=TRUE, sep="")
-print("ERP should thje best variable to explain the system performances.")
-
 # Q3:
 print("Answer of question 3")
+pcDateFrame <- read.table("ComputerData.txt", header=TRUE, sep="")
+print("ERP should thje best variable to explain the system performances.")
 print("ERP will be my choice to explain the value of PRP, since from the Summary Statistics, ERP has the highest RPR Correlation value.")
+mld <- lm(PRP~MYCT, data=pcDateFrame)
+summary(mld)
+mld <- lm(PRP~MMIN, data=pcDateFrame)
+summary(mld)
+mld <- lm(PRP~MMAX, data=pcDateFrame)
+summary(mld)
+confint(mld)
+mld <- lm(PRP~CACH, data=pcDateFrame)
+summary(mld)
+mld <- lm(PRP~CGMIN, data=pcDateFrame)
+summary(mld)
+mld <- lm(PRP~CHMAX, data=pcDateFrame)
+summary(mld)
 
 # Q4:
-# Q5:
-carDateFrame <- read.table("Cars2Data.txt", header=TRUE, sep="")
-summary(carDateFrame)
-summaryMld <- lm(mpg~., data=carDateFrame)
-summary(summaryMld)
+plot(pcDateFrame$MMAX, pcDateFrame$PRP, xlab="MMAX", ylab="PRP")
+abline(mld, col="red")
 
-plot(carDateFrame$mpg~carDateFrame$displacement, xlab="cylinders", ylab="mpg")
-mld <- lm(mpg~cylinders, data=carDateFrame)
+# Q6:
+carDateFrame <- read.table("Cars2Data.txt", header=TRUE, sep="")
+summaryMld <- lm(mpg~cylinders, data=carDateFrame)
+summary(summaryMld)
+summaryMld <- lm(mpg~displacement, data=carDateFrame)
+summary(summaryMld)
+summaryMld <- lm(mpg~horsepower, data=carDateFrame)
+summary(summaryMld)
+summaryMld <- lm(mpg~weight, data=carDateFrame)
+summary(summaryMld)
+confint(summaryMld)
+summaryMld <- lm(mpg~acceleration, data=carDateFrame)
+summary(summaryMld)
+summaryMld <- lm(mpg~year, data=carDateFrame)
+summary(summaryMld)
+print("weight will be the variable I have to choose.")
+
+# Q7:
+plot(carDateFrame$mpg~carDateFrame$weight, xlab="weight", ylab="mpg")
+mld <- lm(mpg~weight, data=carDateFrame)
 mld
 summary(mld)
 abline(mld, col="red")
-# Q6:
-print("year will be the variable I have to choose.")
-# Q7:
 
