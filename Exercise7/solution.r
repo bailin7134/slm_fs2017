ddff3f8894e07960a657657e7367c5add8b4514d
bailin7134 <lbai.ee.ethz@gmail.com>
2017-05-10 23:22:33 +0200

    add solution of exercise 7

--- Exercise7/solution.r
+++ Exercise7/solution.r
@@ -1,35 +1,57 @@
-# single regression
-myData <- read.table("ComputerData.txt", header=T)
-#subset < !(is.na(myData["MYCT","MMIN","MMAX","CACH","CGMIN","CHMAX","PRP"]))
-usefulData <- myData[c("MYCT","MMIN","MMAX","CACH","CGMIN","CHMAX")]
-attach(usefulData)
-#set.seed(123)
-#train <- sample(209,104)
-#test <- (1:209)[-train]
+library(FNN)
 
+# Computer data : need to predict PRP.
+computerData = read.table("ComputerData.txt", header=T)
+
+# Remove the variable model (name), vendor (name) and ERP
+usefulData = computerData[, c("MYCT", "MMIN", "MMAX", "CACH", "CGMIN", "CHMAX", "PRP")]
+predictors = names(usefulData)
+summary(usefulData)
+
+# Standardized the values
+means = lapply(usefulData, mean)
+sd = lapply(usefulData, sd)
+usefulData = (usefulData - means) / sd
+summary(usefulData)
+attach(usefulData)
+
+# train <- sample(209, 188)
+# test <- (1:209)[-train]
+# sigReg <- lm(formula = PRP ~ MMAX, data=usefulData[train,])
+# predictSigReg<- predict(sigReg, usefulData[test,], interval="prediction")
+# mseSigReg <- sum((predictSigReg-usefulData[test,]$PRP)^2)/nrow(usefulData[test,])
+
+# train <- sample(209, 188)
+# test <- (1:209)[-train]
+# mulReg <- lm(formula = PRP ~ MYCT + MMIN + MMAX + CACH + CGMIN + CHMAX, data=usefulData[train,])
+# predictMulReg<- predict(mulReg, usefulData[test,], interval="prediction")
+# mseMulReg <- sum((predictMulReg-usefulData[test,]$PRP)^2)/nrow(usefulData[test,])
+
+# single regression
 library(boot)
 set.seed(123)
-cv.error <- rep(0,10)
+cv.errorSigReg <- rep(0,10)
+cv.SigMse <- rep(0,10)
+print("signle regression time")
 for (i in 1:10) {
   glm.mod <- glm(PRP~poly(MMAX,i), data=usefulData)
-  cv.error[i] <- cv.glm(usefulData,glm.mod,K=10)$delta[2]
+  cv.SigMse[i] <- sum((glm.mod$residuals)^2)
+  cv.errorSigReg[i] <- cv.glm(usefulData,glm.mod,K=10)$delta[2]
 }
-cv.error
+cv.SigMse
 
 # multiple regression
 library(boot)
 set.seed(123)
-cv.error <- rep(0,10)
+cv.errorMulReg <- rep(0,10)
+cv.MulMse <- rep(0,10)
 for (i in 1:10) {
   glm.mod <- glm(PRP~poly(MYCT + MMIN + MMAX + CACH + CGMIN + CHMAX,i), data=usefulData)
-  cv.error[i] <- cv.glm(usefulData,glm.mod,K=10)$delta[2]
+  cv.MulMse[i] <- sum((glm.mod$residuals)^2)
+  cv.errorMulReg[i] <- cv.glm(usefulData,glm.mod,K=10)$delta[2]
 }
-cv.error
+cv.MulMse
 
-# Q2
-train <- sample(209, 105)
-test <- (1:209)[-train]
-library(class)
-prediction <- knn(usefulData[train,], usefulData[test,], myData[train,]$PRP, k=5)
-new_prediction <- as.numeric(as.character(prediction))
-mse_predict <- sum((new_prediction - myData[test,]$PRP)^2)/length(new_prediction)
+# Test K-NN model with LOO
+computer.knn = knn.reg(usefulData, test = NULL, y = PRP, k = 2)
+sum(computer.knn$residuals^2)
